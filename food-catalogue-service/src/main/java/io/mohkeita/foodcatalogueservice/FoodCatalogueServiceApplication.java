f82915ea1b773465ffb393d55c30fd73a58b86b6
“mohkeita” <mohkeita13@gmail.com>
2024-01-17 21:39:37 +0100

    add catalogue service

---
+++ food-catalogue-service/src/main/java/io/mohkeita/foodcatalogueservice/FoodCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.foodcatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCatalogueServiceApplication.class, args);
+	}
+
+}
---
+++ food-catalogue-service/src/test/java/io/mohkeita/foodcatalogueservice/FoodCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.foodcatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
