54ede284f55dd696da80dd428f742fcb1c5a134d
“mohkeita” <mohkeita13@gmail.com>
2024-01-17 03:23:06 +0100

    add listing restaurant service

---
+++ restaurantlisting/src/main/java/io/mohkeita/restaurantlisting/RestaurantlistingApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.restaurantlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantlistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantlistingApplication.class, args);
+	}
+
+}
---
+++ restaurantlisting/src/test/java/io/mohkeita/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.restaurantlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantlistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
