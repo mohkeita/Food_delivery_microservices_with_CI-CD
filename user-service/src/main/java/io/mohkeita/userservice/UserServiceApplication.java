6160e42a0981ad2951592a9e1395d5c9799949ac
“mohkeita” <mohkeita13@gmail.com>
2024-01-17 16:05:11 +0100

    add user service

---
+++ user-service/src/main/java/io/mohkeita/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mohkeita.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
